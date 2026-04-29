b4287f3db798673c1542c2b2c45d781cf9980621
Alon Faraj <alonfaraj@gmail.com>
2026-04-29 18:40:44 +0300

    fix pylint R1730 consider-using-min-builtin

--- examples/asyncio_consumer_example.py
+++ examples/asyncio_consumer_example.py
@@ -428,8 +428,7 @@ class ReconnectingExampleConsumer(object):
             self._reconnect_delay = 0
         else:
             self._reconnect_delay += 1
-        if self._reconnect_delay > 30:
-            self._reconnect_delay = 30
+        self._reconnect_delay = min(self._reconnect_delay, 30)
         return self._reconnect_delay
 
 
